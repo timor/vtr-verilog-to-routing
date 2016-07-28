b13dcb140d921e7b48ee3896a64770a84c320a8f
Vidya Sankaranarayanan <vidyashankar0412@gmail.com>
2016-07-28 10:27:35 -0300

    Moved if ABC command last

--- vtr_flow/scripts/run_vtr_flow.pl
+++ vtr_flow/scripts/run_vtr_flow.pl
@@ -434,7 +434,7 @@ if (    $starting_stage <= $stage_idx_abc
 	and $ending_stage >= $stage_idx_abc
 	and !$error_code )
 {
-    my $abc_commands="read $odin_output_file_name; time; resyn; retime; resyn2; resyn2a; resyn3; retime; if -K $lut_size; time; strash; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; write_hie $odin_output_file_name $abc_output_file_name; print_stats";
+    my $abc_commands="read $odin_output_file_name; time; resyn; retime; resyn2; resyn2a; resyn3; retime; time; strash; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; scleanup; time; if -K $lut_size; write_hie $odin_output_file_name $abc_output_file_name; print_stats";
 
     if ($abc_quote_addition) {$abc_commands = "'" . $abc_commands . "'";}
     
